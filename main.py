7a517c636686c3e4f2ebfd79635518ca7fdc968f
ravshan <ravshan.personal@gmail.com>
2024-01-05 19:21:31 +0600

    allocator script

--- main.py
+++ main.py
@@ -9,21 +9,21 @@ MC = 20 # number of repetitions
 BLUEPRINT = {
 "p": (5,), # number of agents
 "n": (4,), # number of bits
-"kcs": ((1,0,0),), # K,C,S parameters
+"kcs": ((3,0,0),(2,2,2)), # K,C,S parameters
 "t": (500,), # life span of organization
-"rho": (0.3,), # correlation
+"rho": (0.3,0.9), # correlation
 
 "nsoc": (4,), # number of social bits
 "deg": (2,),  # two types of degrees
 "net": (3,), # network structures 0=random,1=line,2=cycle,3=ring,4=star
 "xi": (1.0,), # probability of communicating
 "tm": (50,), # memory
-"coord": (0,1,2), # coordination mode 0=decentralized, 1=lateral, 2=hierarchical
+"coord": (0,), # coordination mode 0=decentralized, 1=lateral, 2=hierarchical
 
 "apc": ((2,2,4),), # ALT,PROP,COMP parameters
 "wf": (1.0,), # weight for phi, incentive scheme
 "goals": ((1.0, 1.0),), # goals for incentives and for conformity
-"w": (0.5,), # weight for incentives ; weight for conformity is 1-w
+"w": (1.0, 0.5), # weight for incentives ; weight for conformity is 1-w
 
 "normalize": (True,), # normalizes by global maximum
 "precompute": (True,), # pre-computes performances for all bitstrings
@@ -65,8 +65,10 @@ def main():
 
         # save to files
         params_filename = "".join(f"{k}{v}" for k,v in params.items())
-        np.savetxt("results/perf/" + params_filename + ".csv", performances, delimiter=',', fmt='%10.5f')
-        np.savetxt("results/sync/" + params_filename + ".csv", synchronies, delimiter=',', fmt='%10.5f')
+        params_filename = params_filename.replace(" ", "") + ".csv"
+
+        np.savetxt("results/perf/" + params_filename, performances, delimiter=',', fmt='%10.5f')
+        np.savetxt("results/sync/" + params_filename, synchronies, delimiter=',', fmt='%10.5f')
 
 if __name__=='__main__':
     main()
