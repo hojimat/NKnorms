03a7af3eb2e716be47cb8952d2b81f1a0ee87783
ravshan <ravshan.personal@gmail.com>
2024-01-12 17:43:12 +0600

    updated plotter too

--- helpers/plotter.py
+++ helpers/plotter.py
@@ -1,47 +1,56 @@
+import logging
+from pathlib import Path
+import json
+import itertools
 import numpy as np
-from math import sqrt
 from matplotlib import pyplot as plt
-import os
 
-#T = 400
-#MC = 200
+def plot(dir_path):
+    """Function that plots all files in a directory as curves in a single graph"""
 
-#fspath = input("Enter directory name:\n")
-
-def mainplot(fspath):
-    fpath = fspath
-    all_files = sorted(os.scandir(fpath),key=lambda e: e.name,reverse=True)
-    for file in all_files:
-        with open(file,"r") as f:
+    for file in sorted(dir_path.iterdir(), reverse=True):
+        # parse file and get data
+        with open(file, "r") as f:
             fname = f.name
-            quantum = np.genfromtxt(fname,delimiter=',')[:,50:]
-            MC = quantum.shape[0]
-            T = quantum.shape[1]
-            superposition = np.mean(quantum,axis=0)
-            supersd = np.std(quantum,axis=0)
-            supererr = supersd*2.5758/sqrt(MC)
-            fname = fname.replace(fpath,"").replace(".csv","").replace("/","").replace("..","")
-            plt.plot(list(range(T)),superposition,
-                    label=fname,
-                    linewidth=1.0,
-                    )
+            quantum = np.genfromtxt(fname,delimiter=',')
+            MC, T = quantum.shape
 
-            plt.fill_between(list(range(T)),superposition-supererr,superposition+supererr,
-                            alpha=0.1)
-    fname = "fig/" + fspath.replace("/","") + ".pdf"
-    #plt.legend(loc="lower right",prop={'family':'serif','size':10},bbox_to_anchor=(0,1))
+        # analyze data
+        superposition = np.mean(quantum, axis=0)
+        supersd = np.std(quantum,axis=0)
+        supererr = supersd*2.5758/np.sqrt(MC)
+
+        # generate labels
+        fname = fname.replace(str(dir_path),"").replace(".csv","").replace("/","").replace("..","")
+
+        # plot the curve
+        plt.plot(list(range(T)),superposition, label=fname, linewidth=1.0)
+
+        # plot confidence intervals
+        plt.fill_between(list(range(T)),superposition-supererr,superposition+supererr,
+                        alpha=0.1)
+   
+    # close the plot
     plt.legend(loc="lower right",prop={'family':'serif','size':5})
-    #plt.ylim(0.7,0.95)
-    plt.savefig(fname,bbox_inches="tight")
+    fname = "fig/" + str(dir_path).replace("/","") + ".pdf"
+    plt.savefig(fname, bbox_inches="tight")
     plt.clf()
+    logging.info(f"done: {fname}")
 
-dir0 = "aperf"
-dirnames = []
-for w in ["w1","w7","w5","w0"]:
-    for rho in ["rho3","rho9"]:
-        for kcs in ["kcs300", "kcs121", "kcs331", "kcs222"]:
-            dirnames.append(f"{dir0}/{w}/{rho}/{kcs}/")
+def main():
+    """Main function"""
 
-for fff in dirnames:
-    mainplot(fff)
-    print(fff)
+    logging.basicConfig(level=logging.INFO)
+    with open('structure.json', 'r', encoding='utf-8') as file:
+        data = json.load(file)  
+        structure = data['structure']
+        patterns = data['patterns']
+    
+
+    for path in itertools.product(*structure):
+        p = Path(*path)
+        plot(p)
+
+
+if __name__=='__main__':
+    main()
