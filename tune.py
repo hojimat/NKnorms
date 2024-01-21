41b673666a71f821d95ed5f94f17dffa1eb075b1
ravshan <ravshan.personal@gmail.com>
2024-01-21 17:34:51 +0600

    version submitted to running on jan21

--- tune.py
+++ tune.py
@@ -1,38 +1,31 @@
-"""
-Main script to set parameters and run the simulation
-
-Author:
-Ravshan Hojimat
-University of Klagenfurt
-"""
+"""Test script to tune parameters"""
 
 import logging
-from multiprocessing import Pool
 import numpy as np
 import progressbar
 from models import Nature
 import nkpack as nk
 
 ########
-MC = 1000 # number of repetitions
+MC = 40 # number of repetitions
 BLUEPRINT = {
 "p": (5,), # number of agents
 "n": (4,), # number of bits
-"kcs": ((3,0,0),(2,1,2)), # K,C,S parameters
+"kcs": ((3,0,0),(2,2,2)), # K,C,S parameters
 "t": (500,), # life span of organization
-"rho": (0.9,0.6), # correlation
+"rho": (0.9,), # correlation
 
 "nsoc": (4,), # number of social bits
 "deg": (2,),  # two types of degrees
-"net": (1,2,3,4), # network structures 0=random,1=line,2=cycle,3=ring,4=star
+"net": (3,), # network structures 0=random,1=line,2=cycle,3=ring,4=star
 "xi": (1.0,), # probability of communicating
 "tm": (50,), # memory
-"coord": (0,1,2), # coordination mode 0=decentralized, 1=lateral, 2=hierarchical
+"coord": (2,), # coordination mode 0=decentralized, 1=lateral, 2=hierarchical
 
-"apc": ((2,2,2), (4,2,2)), # ALT,PROP,COMP parameters
+"apc": ((2,2,2),), # ALT,PROP,COMP parameters
 "wf": (1.0,), # weight for phi, incentive scheme
-"goals": ((1.0, 1.0), (1.0, 0.6)), # goals for incentives and for conformity
-"w": (1.0, 0.5), # weight for incentives ; weight for conformity is 1-w
+"goals": ((1.0, 0.8), (1.0, 0.6)), # goals for incentives and for conformity
+"w": (1.0,), # weight for incentives ; weight for conformity is 1-w
 
 "normalize": (True,), # normalizes by global maximum
 "precompute": (True,), # pre-computes performances for all bitstrings
@@ -40,7 +33,7 @@ BLUEPRINT = {
 
 ########
 
-def run_simulation(parameters, bar_, mc_):
+def run_simulation(parameters):
     """A set of instructions for a single iteration"""
     nature = Nature(**parameters)
     #np.random.seed()
@@ -48,23 +41,17 @@ def run_simulation(parameters, bar_, mc_):
     nature.play()
     perfs = nature.organization.performances.mean(axis=1)
     syncs = nature.organization.synchronies
-    bar_.update(mc_)
     return perfs, syncs
 
 
 if __name__=='__main__':
     logging.basicConfig(level=logging.WARNING)
     for params in nk.variate(BLUEPRINT):
-        pbar = progressbar.ProgressBar(max_value=MC)
 
-        def worker(i):
-            """A worker function that calls iterations in a Pool"""
-            return run_simulation(params, pbar, i)
-
-        pbar.start()
-        with Pool(8) as pool:
-            quantum = pool.map(worker, range(MC))
-        pbar.finish()
+        quantum = []
+        for i in range(MC):
+            print(i)
+            quantum.append(run_simulation(params))
 
         # T x MC array of mean performance and synchrony of an
         # organization at every period for MC repetitions
