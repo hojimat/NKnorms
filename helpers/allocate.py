c4975a8a6d0f34f5cea835747dde259b10acdbc6
ravshan <ravshan.personal@gmail.com>
2024-01-26 12:01:48 +0600

    updated magic copy tool

--- helpers/allocate.py
+++ helpers/allocate.py
@@ -18,28 +18,39 @@ def scaffold(dirs):
         p = Path(*path)
         p.mkdir(parents=True, exist_ok=True)
 
-def categorize(top_dir, patterns):
+def categorize(dirs, patterns):
     """Move files to the subdirectories by matching patterns"""
 
-    top_dir = Path(top_dir)
-    for file in top_dir.rglob('*'):
-        if file.is_file():
-            dest_path = top_dir
+    # first element of dirs in json is always root or top dir
+    for top_dir in dirs[0]:
+        # create a path object
+        top_dir = Path(top_dir)
 
-            for dir_name, pattern in patterns.items():
-                if re.search(pattern, file.name):
-                    dest_path /= dir_name
+        # get the flat list of all possible directory names
+        categories = [z for _ in dirs[1:] for z in _]
+
+        # sort files into categories by patterns
+        for file in top_dir.rglob('*'):
+            # ignore directories
+            if not file.is_file():
+                continue
+
+            # build path from scratch
+            dest_path = top_dir
+            for cat in categories:
+                if re.search(patterns[cat], file.name):
+                    dest_path /= cat
 
             if dest_path != top_dir:
                 file.rename(dest_path / file.name)
 
-def flatten(root_dir):
+def flatten(dirs):
     """Moves all files back to the root directory and removes empty directories"""
-
-    root_dir = Path(root_dir)
-    for file in root_dir.rglob('*'):
-        if file.is_file():
-            file.rename(root_dir / file.name)
+    for root_dir in dirs[0]:
+        root_dir = Path(root_dir)
+        for file in root_dir.rglob('*'):
+            if file.is_file():
+                file.rename(root_dir / file.name)
 
 def main():
     """Main function"""
@@ -49,9 +60,9 @@ def main():
         structure = data['structure']
         patterns = data['patterns']
 
-    scaffold(structure)
-    categorize('perf', patterns)
-    #flatten('sync')
+    #scaffold(structure)
+    categorize(structure, patterns)
+    #flatten(structure)
 
 
 if __name__=='__main__':
